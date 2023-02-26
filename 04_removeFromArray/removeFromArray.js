7131590a4850a88826eb5511d3d24e636645078c
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 14:53:52 +0100

    Pass 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function( arr, ...removables) {
+  function checkVal(val){
+    for (let removable of removables){
+      if (val === removable){
+        return false;
+        break;
+      }
+    }
+    return true;
+  }
+  return arr.filter(checkVal);
 };
 
 // Do not edit below this line
