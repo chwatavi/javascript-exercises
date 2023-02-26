0a599204a57d245cbf85bef5af940199ec942420
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 15:09:53 +0100

    Pass 05 and 06

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  function verifyInput(n){
+    return (typeof n) == 'number' && n >= 0;
+  }
+  if (!verifyInput(a) || !verifyInput(b)){
+    return 'ERROR';
+  }
+  if (a > b){
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+  sum = 0;
+  for (num = a; num <= b; num ++){
+    sum += num;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+  leap = false;
+  if (yr % 4 == 0){
+    leap = true;
+    if (yr % 100 == 0){
+      leap = false;
+        if (yr % 400 == 0){
+          leap = true;
+        }
+    }
+  }
+  return leap;
 };
 
 // Do not edit below this line
