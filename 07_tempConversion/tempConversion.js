bed6c45bc05e202d82b028c324fe6075f7f1135b
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 15:30:46 +0100

    Pass 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFah) {
+  let tempCel = (tempFah - 32) * 5 / 9;
+  return Math.round(tempCel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCel) {
+  let tempFah = tempCel * 9 / 5 + 32;
+  return Math.round(tempFah * 10) / 10;
 };
 
 // Do not edit below this line
