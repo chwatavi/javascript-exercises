b8bac5ca686ac07a9431a045c34178cbc2f69847
chwatavi <44669003+chwatavi@users.noreply.github.com>
2023-02-26 14:44:42 +0100

    Pass 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let charList = string.split('');
+  let output = '';
+  while (charList.length > 0){
+    output += charList.pop();
+  }
+  return output;
 };
 
 // Do not edit below this line
